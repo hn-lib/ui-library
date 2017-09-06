7042677c6fb40ba630fcfbe320e8135d64f7287c
Liao Ralken <lira@coresystems.ch>
2017-09-06 15:56:10 +0800

    - Commit the calendar ui library that used in Vip service module.

---
+++ vip-calendarview/src/main/java/com/henong/android/widget/calendar/theme/ISmoothListener.java
@@ -0,0 +1,43 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright (c) 2016 Shanghai HeNong Network Technology Co.,Ltd.All rights reserved.
+ *
+ * This source code has been made available to you by HeNong on
+ * AS-IS.Anyone receiving this source is licensed under
+ * HeNong copyrights to use it in any way he or she deems fit,including
+ * copying it,modifying it,compiling it,and redistributing it either with
+ * or without modifictions.
+ *
+ * Any person who transfers this source code or any derivative work must
+ * include the HeNong copyright notice, this paragraph,and the preceding
+ * two paragraphs in the transferred software.
+ *
+ * Copyright (c) 2016 Shanghai HeNong Network Technology Co.,Ltd.All rights reserved.
+ * LICENSED MATERIAL - PROGRAM PROPERTY OF HENONG
+ */
+package com.henong.android.widget.calendar.theme;
+
+/**
+ *
+ * FILE: ISmoothListener.java
+ *
+ * MODULE: com.henong.android.widget.calendar.theme
+ *
+ * PURPOSE: TODO
+ *
+ * AUTHOR(S): Joker.Chen
+ *
+ * GROUP: R&D
+ *
+ * DATE CREATED: 2016-12-3
+ *
+ * REFERENCE DOCUMENT ID: 
+ *
+ * MODIFICATIONS:
+ * Date       user Name   Description
+ * 2016-12-3    Joker       Create this file
+ */
+
+public interface ISmoothListener {
+	void afterSmoothedEvent();
+}
